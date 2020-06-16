4813290fb41649a9a93fe69305a650fc45de11f7
zhiyuanbest <dingjianylx@163.com>
2020-06-16 19:00:59 +0800

    first commit

---
+++ src/test/java/com/dingj/code/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingj.code.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
